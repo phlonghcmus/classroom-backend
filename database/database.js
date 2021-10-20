c4db2a9edb02d180674701f4af7cdb046b47d43b
phlonghcmus <phlonghcmus@gmail.com>
2021-10-20 15:36:37 +0700

    fix bug

--- database/database.js
+++ database/database.js
@@ -4,9 +4,11 @@ let database;
 const client = new MongoClient("mongodb+srv://admin:18120452@classroommanager.jgw7a.mongodb.net/test",{ useUnifiedTopology: true });
 async function run() {
         // Connect the client to the server
-        await client.connect();
+        await client.connect()
+        .catch(error => { throw error})
         // Establish and verify connection
-       database= await client.db("ClassroomManager");
+       database= await client.db("ClassroomManager")
+       .catch(error => { throw error})
         console.log("Connected successfully to server");
 }
 
